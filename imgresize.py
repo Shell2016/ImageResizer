1b81931d92b33d357b00050dd5e77846fbf79dc4
Michael Shell <bvg2003@bk.ru>
2021-07-15 19:44:49 +0300

    final version

--- imgresize.py
+++ imgresize.py
@@ -12,18 +12,26 @@ def img_resize(in_folder, out_folder):
             try:
                 with Image.open(os.path.join(in_folder, filename)) as im:
                     im.thumbnail(size)
-                    im.save(os.path.join(out_folder, f'{in_folder}_{index}.jpg'))
+                    im.save(os.path.join(
+                        out_folder, f'{in_folder}_{index}.jpg'))
                     im.thumbnail(size_thmb)
-                    im.save(os.path.join(out_folder, f'{in_folder}_min_{index}.jpg'))
+                    im.save(os.path.join(
+                        out_folder, f'{in_folder}_min_{index}.jpg'))
             except OSError:
                 print('OSError')
 
 
-size = 1300, 1300
-size_thmb = 512, 512
-for input_folder in sys.argv[1:]:
+size = 1400, 1400
+size_thmb = 400, 400
+for input_folder in sys.argv[1:]:	
     output_folder = f'{input_folder}_min'
     img_resize(input_folder, output_folder)
+
+
+
+
+
+
 # if not os.path.exists(output_folder):
 #     os.makedirs(output_folder)
 # for filename in os.listdir(input_folder):
